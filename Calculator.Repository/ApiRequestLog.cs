2a5457a1e6f10fb824db28f18e77c3ad438b2aa7
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-20 01:06:10 +0000

    Add repo project

---
+++ Calculator.Repository/ApiRequestLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator.Repository
+{
+    public class ApiRequestLog
+    {
+        public string ApiRequestEndpoint { get; set; }
+        public string IPAddress { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+    }
+}
---
+++ Calculator.Repository/CalculatorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Calculator.Repository
+{
+    public class CalculatorContext : DbContext
+    {
+        public CalculatorContext(DbContextOptions<CalculatorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiRequestLog> ApiRequestLogs { get; set; }
+    }
+
+}
