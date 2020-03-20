4920a8f036229cfea0c7d48ac75d05af42944fed
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-20 01:06:33 +0000

    Add service project

---
+++ Calculator.Service/CalculatorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace Calculator.Service
+{
+    public class CalculatorService : ICalculator
+    {
+
+        public float CalculateStringExpression(string expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException(nameof(expression));
+
+            DataTable dt = new DataTable();
+            var v = dt.Compute(expression, "");
+            return float.Parse(v.ToString());
+        }
+    }
+}
---
+++ Calculator.Service/ICalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator.Service
+{
+    public interface ICalculator
+    {
+        float CalculateStringExpression(string expression);
+    }
+}
