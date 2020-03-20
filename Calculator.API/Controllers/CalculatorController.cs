080c2df4e015ae467099e3bc93eeb3e3c7b81e5d
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-20 01:07:08 +0000

    Add controller

---
+++ Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> logger;
+        private readonly ICalculator calculator;
+
+        public CalculatorController(ILogger<CalculatorController> logger, ICalculator calculator)
+        {
+            this.logger = logger;
+            this.calculator = calculator;
+        }
+
+        [HttpPost]
+        public float Calculate([FromBody] string expression)
+        {
+            return calculator.CalculateStringExpression(expression);
+        }
+    }
+}
